0d37c163b1e73b356c4eb1682482a2315cd3638d
vedeluca <vedeluca@comcast.net>
2022-10-08 21:36:14 -0400

    fixed toughness

--- sheets-code.js
+++ sheets-code.js
@@ -1,4 +1,4 @@
-DICE_CHAIN = {
+DICE_OBJ = {
 	"-": 0,
 	"d4": 4,
 	"d6": 6,
@@ -7,16 +7,65 @@ DICE_CHAIN = {
 	"d12": 12
 };
 
+DICE_ARR = ["-", "d4", "d6", "d8", "d10", "d12"];
+
+BABIROG = "Babirog";
+HUMAN = "Human";
+MONODO = "Monodo";
+SQUIBBEL = "Squibbel";
+TOADYBOG = "Toadybog";
+
+
+function GET_SIZE(race) {
+	let size = 0;
+	if ([SQUIBBEL, TOADYBOG].includes(race))
+		size--;
+	return size;
+}
+
 function GET_PACE() {
 	return 6;
 }
 
 function GET_PARRY(fighting) {
-	return 2 + DICE_CHAIN[fighting] / 2;
+	return 2 + DICE_OBJ[fighting] / 2;
 }
 
-function GET_TOUGHNESS(vigor, armor) {
-	const halfVigor = DICE_CHAIN[vigor] / 2;
-	const total = 2 + halfVigor + armor
+function GET_TOUGHNESS(vigor, armor, race, size) {
+	const halfVigor = DICE_OBJ[vigor] / 2;
+	let total = 2 + halfVigor + parseInt(armor) + parseInt(size);
+	if (race == MONODO)
+		total++;
 	return `${total}(${armor})`;
+}
+
+function GET_AGILITY(die, mod) {
+	return getDice(die, mod, 0, "d4");
+}
+
+function GET_SMARTS(die, mod) {
+	return getDice(die, mod, 0, "d4");
+}
+
+function GET_SPIRIT(die, mod) {
+	return getDice(die, mod, 0, "d4");
+}
+
+function GET_STRENGTH(die, mod) {
+	return getDice(die, mod, 0, "d4");
+}
+
+function GET_VIGOR(die, mod) {
+	return getDice(die, mod, 0, "d4");
+}
+
+function getDice(die, mod, bonus, min) {
+	let index = DICE_ARR.indexOf(die) + bonus;
+	if (index < min)
+		index = min;
+	if (index > 5)
+		index = 5;
+	if (mod)
+		return `${DICE_ARR[index]} + ${mod}`;
+	return DICE_ARR[index];
 }
